0f58b20f4b30265280e5b64d830fc134c92bae9e
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2024-09-18 14:46:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component{
         <LifeCycleMethods />} */}
        
         {/* <ImageGallery /> */}
-       {/* <FCOne /> */}
+       <FCOne />
         <CounterFn />
       
 
